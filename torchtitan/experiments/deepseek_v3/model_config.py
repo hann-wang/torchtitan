4ff7d0f9758a513aec18e34bcb96434178fc092b
Han Wang <Han.Wang@amd.com>
2025-06-17 07:52:43 +0000

    update v3lite model size to 20b

--- torchtitan/experiments/deepseek_v3/model_config.py
+++ torchtitan/experiments/deepseek_v3/model_config.py
@@ -301,13 +301,13 @@ deepseek_v2_dim10752_config = ModelArgs(
 deepseek_v3_lite_config = ModelArgs(
     vocab_size=102400,
     hidden_size=2048,
-    intermediate_size=10752,  # the original 10944 is not divisible by 128
+    intermediate_size=10240,  # the original 10944 is not divisible by 128
     moe_intermediate_size=1280,
     num_hidden_layers=27,
     num_attention_heads=16,
     num_key_value_heads=16,
-    n_shared_experts=1,
-    n_routed_experts=64,
+    n_shared_experts=2,
+    n_routed_experts=96,
     first_k_dense_replace=1,
 )
 
